0b3a92fc43f2a6ee7ea06fd833aa29b2d261ffd2
su9arfree <tompham3385@gmail.com>
2019-08-15 13:29:43 +0700

    done

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -5,6 +5,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_accessor :values
+  def initialize
+  end
+
+  def roll(num)
+    @values = []
+    num.times do
+      @values << rand(5)+1
+    end
+  end
+  end
+
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
